23c16e4e97683db38627e592311bdc26ed84d626
SUVANSH DUREJA <80078177+SuvanshD@users.noreply.github.com>
2025-01-01 04:13:52 +0530

    Time: 36 ms (24.62%), Space: 22.3 MB (56.23%) - LeetHub

--- 1843-number-of-rectangles-that-can-form-the-largest-square/1843-number-of-rectangles-that-can-form-the-largest-square.cpp
+++ 1843-number-of-rectangles-that-can-form-the-largest-square/1843-number-of-rectangles-that-can-form-the-largest-square.cpp
@@ -3,21 +3,41 @@ class Solution
     public:
         int countGoodRectangles(vector<vector < int>> &rectangles)
         {
-            vector <int> arr;
+            //1st Approach
+            // vector <int> arr;
 
-            for(auto it:rectangles)
-            {
-                int minimum = min(it[0],it[1]);
-                arr.push_back(minimum);
-            }
+            // for(auto it:rectangles)
+            // {
+            //     int minimum = min(it[0],it[1]);
+            //     arr.push_back(minimum);
+            // }
 
-            int highest = *max_element(arr.begin(),arr.end());
+            // int highest = *max_element(arr.begin(),arr.end());
             
+            // int maxLen=0;
+
+            // for(auto it:arr)
+            // {
+            //     if(it>=highest) maxLen++;
+            // }
+
+            //2nd Approach
+            int highest=0;
             int maxLen=0;
 
-            for(auto it:arr)
+            for(auto &it:rectangles)
             {
-                if(it>=highest) maxLen++;
+                int minimum = min(it[0],it[1]);
+
+                if(minimum>highest)
+                {
+                    highest=minimum;
+                    maxLen=1;
+                }
+                else if(minimum==highest)
+                {
+                    maxLen++;
+                }
             }
 
             return maxLen;
