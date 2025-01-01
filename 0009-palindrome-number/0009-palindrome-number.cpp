4b776051a7630290ffd831dbdf7098926c631f04
SUVANSH DUREJA <80078177+SuvanshD@users.noreply.github.com>
2025-01-02 00:34:03 +0530

    Time: 0 ms (100%), Space: 8.7 MB (34.62%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,24 +1,18 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        if (x >= INT_MAX || x <= INT_MIN  ) 
-        {
-            return 0;
-        }
+        if(x<0) return false;
         
-        long final=0;
-        int n=abs(x);
+        int final=0;
+        int last_digit = 0;
+        int n=x;
         while(n!=0)
         {
-            int last_digit = n%10;
-
-            // if (final >= INT_MAX / 10 ) {
-            //     return false;
-            // }
-            // if (final <= INT_MIN / 10 ) {
-            //     return false;
-            // }
-
+            last_digit = n%10;
+            if (final >= INT_MAX/10 || final <= INT_MIN/10  ) 
+            {
+                return 0;
+            }
             final=(final*10)+last_digit;
             n=n/10;
         }
