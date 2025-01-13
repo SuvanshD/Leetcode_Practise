65a0a14a47703d5d35e977a62fccedf056f48cba
SUVANSH DUREJA <80078177+SuvanshD@users.noreply.github.com>
2025-01-13 19:56:37 +0530

    Time: 0 ms (100%), Space: 23.2 MB (61.48%) - LeetHub

--- 3242-count-elements-with-maximum-frequency/3242-count-elements-with-maximum-frequency.cpp
+++ 3242-count-elements-with-maximum-frequency/3242-count-elements-with-maximum-frequency.cpp
@@ -1,26 +1,44 @@
 class Solution {
 public:
-    int maxFrequencyElements(vector<int>& nums) {
+    // int maxFrequencyElements(vector<int>& nums) {
         
-        int highest=0;
-        int total=0;
+    //     int highest=0;
+    //     int total=0;
 
-        unordered_map <int,int> mpp;
+    //     unordered_map <int,int> mpp;
 
-        for(int i=0;i<nums.size();i++)
-        {
-            mpp[nums[i]]++;
-            if(mpp[nums[i]]>highest) highest = mpp[nums[i]];
+    //     for(int i=0;i<nums.size();i++)
+    //     {
+    //         mpp[nums[i]]++;
+    //         if(mpp[nums[i]]>highest) highest = mpp[nums[i]];
+    //     }
+
+    //     for(auto x:mpp)
+    //     {
+    //         if(x.second==highest)
+    //         {
+    //             total+=highest;
+    //         }
+    //     }
+
+    //     return total;
+    // }
+    int maxFrequencyElements(vector<int>& nums) {
+    unordered_map<int, int> frequencyMap;
+    int highestFrequency = 0, total = 0;
+
+    // Count frequencies and determine the highest frequency in one pass
+    for (int num : nums) {
+        int freq = ++frequencyMap[num];
+        if (freq > highestFrequency) {
+            highestFrequency = freq;
+            total = freq; // Reset total as a new highest frequency is found
+        } else if (freq == highestFrequency) {
+            total += freq; // Add to total if another element matches highest frequency
         }
-
-        for(auto x:mpp)
-        {
-            if(x.second==highest)
-            {
-                total+=highest;
-            }
-        }
-
-        return total;
     }
+
+    return total;
+}
+
 };
