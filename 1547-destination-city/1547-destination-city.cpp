2389e5bd53d7120d085022932e64eb8d2dd19b72
SUVANSH DUREJA <80078177+SuvanshD@users.noreply.github.com>
2025-01-26 23:02:50 +0530

    Time: 3 ms (48.05%), Space: 15.7 MB (33.79%) - LeetHub

--- 1547-destination-city/1547-destination-city.cpp
+++ 1547-destination-city/1547-destination-city.cpp
@@ -2,20 +2,37 @@ class Solution {
 public:
     string destCity(vector<vector<string>>& paths) {
         
+        // set<string> start_city;
+        // set<string> final_city;
+
+        // for(auto it:paths)
+        // {
+        //     start_city.insert(it[0]);
+        //     final_city.insert(it[1]);
+        // }
+
+        // for(auto it:final_city)
+        // {
+        //     if(start_city.find(it)==start_city.end())
+        //     {
+        //         return it;
+        //     }
+        // }
+
         set<string> start_city;
-        set<string> final_city;
+        //set<string> final_city;
 
         for(auto it:paths)
         {
             start_city.insert(it[0]);
-            final_city.insert(it[1]);
+            //final_city.insert(it[1]);
         }
 
-        for(auto it:final_city)
+        for(auto it:paths)
         {
-            if(start_city.find(it)==start_city.end())
+            if(start_city.find(it[1])==start_city.end())
             {
-                return it;
+                return it[1];
             }
         }
 
