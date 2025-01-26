d9c274e4e1da8de4dd32497c4a28aa64d7deebd7
SUVANSH DUREJA <80078177+SuvanshD@users.noreply.github.com>
2025-01-26 23:07:06 +0530

    Time: 0 ms (100%), Space: 16 MB (17.67%) - LeetHub

--- 1547-destination-city/1547-destination-city.cpp
+++ 1547-destination-city/1547-destination-city.cpp
@@ -19,18 +19,32 @@ public:
         //     }
         // }
 
-        unordered_set<string> start_city;
-        //set<string> final_city;
+        // unordered_set<string> start_city;
+        // //set<string> final_city;
 
+        // for(auto it:paths)
+        // {
+        //     start_city.insert(it[0]);
+        //     //final_city.insert(it[1]);
+        // }
+
+        // for(auto it:paths)
+        // {
+        //     if(start_city.find(it[1])==start_city.end())
+        //     {
+        //         return it[1];
+        //     }
+        // }
+
+        unordered_set<string> start_city;
         for(auto it:paths)
         {
             start_city.insert(it[0]);
-            //final_city.insert(it[1]);
         }
 
         for(auto it:paths)
         {
-            if(start_city.find(it[1])==start_city.end())
+            if(!start_city.count(it[1]))
             {
                 return it[1];
             }
