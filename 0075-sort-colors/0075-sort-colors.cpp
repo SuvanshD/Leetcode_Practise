e6ae0e9582173bbeec7466833220538bf18685db
SUVANSH DUREJA <80078177+SuvanshD@users.noreply.github.com>
2025-03-04 16:53:20 +0530

    Time: 0 ms (100%), Space: 11.6 MB (68.84%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,6 +1,37 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
+        int sum0=0;
+        int sum1=0;
+        int sum2=0;
+
+        for(int i=0;i<nums.size();i++)
+        {
+            if (nums[i]==0)
+            {
+                sum0++;
+            }
+            else if(nums[i]==1)
+            {
+                sum1++;
+            }
+            else
+            {
+                sum2++;
+            }
+        }
+
+        for(int i=0;i<sum0;i++)
+        {
+            nums[i]=0;
+        }
+        for(int i=sum0;i<sum0+sum1;i++)
+        {
+            nums[i]=1;
+        }
+        for(int i=sum0+sum1;i<sum0+sum1+sum2;i++)
+        {
+            nums[i]=2;
+        }
     }
 };
