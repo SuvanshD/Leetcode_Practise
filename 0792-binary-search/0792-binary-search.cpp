0cd8a9946ee64ff76a1009a7b2829529ee48551f
SUVANSH DUREJA <80078177+SuvanshD@users.noreply.github.com>
2025-04-03 19:06:03 +0530

    Time: 0 ms (100%), Space: 31.3 MB (45.68%) - LeetHub

--- 0792-binary-search/0792-binary-search.cpp
+++ 0792-binary-search/0792-binary-search.cpp
@@ -1,19 +1,21 @@
 class Solution {
 public:
+    int func(vector <int> & nums,int low, int high , int target)
+    {
+        if (low>high) return -1;
+
+        int mid = (low+high)/2;
+
+        if(nums[mid]==target) return mid;
+        else if (target>nums[mid]) return func(nums,mid+1,high,target);
+        else return func(nums,low,mid-1,target);
+    }
+
     int search(vector<int>& nums, int target) {
         int n = nums.size();
         int low = 0;
         int high = n-1;
 
-        while(low<=high)
-        {
-            int mid = (low+high)/2;
-
-            if(nums[mid]==target) return mid;
-            else if (target>nums[mid]) low = mid + 1;
-            else high = mid - 1;
-        }
-
-        return -1;
+        return func(nums,low,high,target);
     }
 };
